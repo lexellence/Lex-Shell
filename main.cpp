272f317cb8fdf8f27b6767b92bdbb330654007ce
DavidLeksen <lexellence@protonmail.com>
2019-11-20 15:16:12 -0500

    organized and moved some code into functions; edited comments and error messages; changed wordList into a Command struct, modified all functions to work with Command struct; modified the separate function to print error if MAX_COMMAND_LENGTH is exceeded rather than cutting off the end and processing a shortened command; moved handling of 'cd' command to its own function, fixed a bug where chdir wasn't being called when ~ was being expanded, added error checking for chdir, added changing to root directory if no arguments are sent to 'cd' ('cd /' also works), added changing to last working directory with 'cdl' (convenient if you accidentally cd to root);

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/**************************************************************************************\
+** File: main.cpp
+** Project: Lex - Custom Linux Shell
+** Author: David Leksen
+** Date:
+\**************************************************************************************/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,129 +11,277 @@
 #include <sys/wait.h>
 
 // Limit command inputLength to make sure SeparateIntoWords works correctly:
-//		std::string::size_type is converted to int
-//		If a different splitting algorithm was used, this could be removed.
+//		std::string::size_type is converted to int, which is potentially a narrowing conversion.
+//		So we must put a limit on the value, but we could make this limit as high as INT_MAX if we wanted.
+//		If a different splitting algorithm was used without this conversion, this limit could be removed.
 const std::string::size_type MAX_COMMAND_LENGTH{ 1024 };
 
-// Console text color formatting codes
-const std::string defaultStyle("\033[0m");
-const std::string greenOnDefault("\033[0;32;49m");
-const std::string blueOnDefault("\033[0;34;49m");
-const std::string greenOnDefaultBold("\033[1;32;49m");
-const std::string blueOnDefaultBold("\033[1;34;49m");
+// Console text formatting
+const std::string defaultStyle{ "\033[0m" };
+const std::string greenOnDefault{ "\033[0;32;49m" };
+const std::string blueOnDefault{ "\033[0;34;49m" };
+const std::string greenOnDefaultBold{ "\033[1;32;49m" };
+const std::string blueOnDefaultBold{ "\033[1;34;49m" };
+const std::string shellName{ "lex" };
 
-void MainLoop();
-void SeparateIntoWords(const std::string& inputString, std::vector<std::string>& outputWordList);
+struct Command
+{
+	std::string name;
+	std::vector<std::string> arguments;
+};
+
+std::string lastWorkingDirectory;
+
+//+------------------------\----------------------------------
+//|		   Commands		   |
+//\------------------------/----------------------------------
+void ExecuteCommand(const Command& command);
+void ChangeWorkingDirectory(const std::string& directory);
+void ExecuteExternalApp(const Command& command);
+
+//+------------------------\----------------------------------
+//|		   Utility		   |
+//\------------------------/----------------------------------
+void ClearTerminal();
+void PrintPrompt();
+void SeparateIntoCommand(const std::string& input, Command& commandOut);
+void PrintCommand(const Command& command);
 void PrintWordList(const std::vector<std::string>& wordList);
-void ExecuteExternalApp(const std::vector<std::string>& pathAndArgs);
 void GetWorkingDirectory(std::string& pathOutput);
 
+//+------------------------\----------------------------------
+//|			 Main		   |
+//\------------------------/----------------------------------
+void DoShell();
 int main()
 {
 	try
 	{
-		MainLoop();
+		DoShell();
 	}
 	catch(const std::exception & e)
 	{
-		std::cerr << "Fatal Exception: " << e.what() << std::endl;
+		std::cerr << shellName << ": Fatal Exception: " << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
-
-void MainLoop()
+void DoShell()
 {
-	// Clear terminal
-	ExecuteExternalApp({ "clear" });
-
+	ClearTerminal();
+	GetWorkingDirectory(lastWorkingDirectory);
 	while(true)
 	{
-		// Display shell prompt including current directory
-		std::string workingDirectoryString;
-		GetWorkingDirectory(workingDirectoryString);
-		std::cout << greenOnDefaultBold << "lex"
-			<< defaultStyle << ':'
-			<< blueOnDefaultBold << '~' << workingDirectoryString
-			<< defaultStyle << "$ ";
+		PrintPrompt();
 
-		// Get list of words from command-line
-		std::string inputLineString;
-		getline(std::cin, inputLineString);
-		if(inputLineString.length() > MAX_COMMAND_LENGTH)
+		// Get input from command-line and format it into a command and arguments
+		Command currentCommand;
 		{
-			std::cerr << "Exceeded max " << MAX_COMMAND_LENGTH << " characters." << std::endl;
-			continue;
+			std::string inputLine;
+			getline(std::cin, inputLine);
+
+			//************************************************  
+			//TODO: separate into list of commands separated by "&&" 
+			//************************************************
+			SeparateIntoCommand(inputLine, currentCommand);
 		}
-		std::vector<std::string> wordList;
-		SeparateIntoWords(inputLineString, wordList);
-		if(wordList.empty())
+		if(currentCommand.name.empty())
 			continue;
 
-		//SeparateIntoCommands(wordList, wordListVector);
-
-		// Execute built-in command if one exists, otherwise try executing external application
-		if(wordList[0] == "exit")
-			return;
-		else if(wordList[0] == "cd")
-		{
-			if(wordList.size() != 2)
-			{
-				std::cerr << "Invalid number of arguments. Usage: $ cd <dir>" << std::endl;
-				continue;
-			}
-			else
-			{
-				// If we need to expand tilde to home directory
-				if(wordList[1][0] == '~')
-				{
-					// Try to get home directory
-					std::string directoryString;
-					char* homeDirectory = getenv("HOME");
-
-					// If home directory exists, start the directory path with it
-					if(homeDirectory)
-						directoryString += homeDirectory;
-
-					// Add the rest of the directory path
-					directoryString += wordList[1].substr(1, wordList[1].length() - 1);
-				}
-				else
-				{
-					// Attempt to change working directory without any tilde expansion
-					chdir(wordList[1].c_str());
-				}
-			}
-		}
-		else
-			ExecuteExternalApp(wordList);
+		ExecuteCommand(currentCommand);
 	}
 }
-void SeparateIntoWords(const std::string& inputString, std::vector<std::string>& outputWordList)
-{
-	// Make sure output is empty
-	outputWordList.clear();
 
-	// Don't process empty input
-	if(inputString.empty())
+//+------------------------\----------------------------------
+//|		   Commands		   |
+//\------------------------/----------------------------------
+void ExecuteCommand(const Command& command)
+{
+	if(command.name == "exit")
+		return;
+	else if(command.name == "cd")
+	{
+		if(command.arguments.size() > 1)
+			std::cerr << shellName << ": cd: too many arguments" << std::endl;
+		else if(command.arguments.empty())
+			ChangeWorkingDirectory("");
+		else
+			ChangeWorkingDirectory(command.arguments[0]);
+	}
+	else if(command.name == "cdl")
+	{
+		if(!command.arguments.empty())
+			std::cerr << shellName << ": cdl: too many arguments" << std::endl;
+		else
+			ChangeWorkingDirectory(lastWorkingDirectory);
+	}
+	else
+		ExecuteExternalApp(command);
+}
+void ChangeWorkingDirectory(const std::string& directory)
+{
+	// If no directory specified, change to root directory
+	std::string finalDirectory;
+	if(directory.empty())
+		finalDirectory = "/";
+
+	// If first char is ~ expand it to home directory
+	else if(directory[0] == '~')
+	{
+		// Try to get home directory
+		char* home{ getenv("HOME") };
+
+		// If home directory does not exist, bail
+		if(!home)
+		{
+			std::cerr << shellName << ": cd: ~: Failed to find home directory" << std::endl;
+			return;
+		}
+
+		// Put directory string together
+		finalDirectory = home;
+		if(directory.size() > 1)
+			finalDirectory += directory.substr(1, directory.length() - 1);
+	}
+
+	// Unmodified directory
+	else
+		finalDirectory = directory;
+
+	// Change working directory
+	std::string savedWorkingDirectory;
+	GetWorkingDirectory(savedWorkingDirectory);
+	bool error;
+	errno = 0;
+	error = chdir(finalDirectory.c_str());
+	if(error)
+	{
+		std::string message{ shellName + ": cd: \'" + finalDirectory + "\'" };
+		perror(message.c_str());
+	}
+	else
+		lastWorkingDirectory = savedWorkingDirectory;
+}
+
+void ExecuteExternalApp(const Command& command)
+{
+	if(command.name.empty())
 		return;
 
-	// Convert length to signed integer so that (currentChar - currentWordStart + 1) works below
-	int inputLength;
-	if(inputString.length() > MAX_COMMAND_LENGTH)
-		inputLength = (int)MAX_COMMAND_LENGTH;
+	errno = 0;
+	pid_t child_pid;
+	child_pid = fork();
+
+	// If fork failed, print error
+	if(child_pid < 0)
+	{
+		std::string message{ shellName + ": " + command.name };
+		perror(message.c_str());
+	}
+
+	if(child_pid == 0)
+	{
+		// This is the child process
+
+		// Convert command to c-style string list 
+		std::vector<char*> cStyleStringList;
+
+		// argv[0] is always the app name		
+		//************************************************  
+		//TODO: separate path from app name (detecting last '/')  and only put name in argv[0] while sending the full path to execvp 
+		//************************************************
+		cStyleStringList.emplace_back(const_cast<char*>(command.name.c_str()));
+
+		// Add arguments
+		for(auto const& arg : command.arguments)
+			cStyleStringList.emplace_back(const_cast<char*>(arg.c_str()));
+
+		// exec expects null-terminated array
+		cStyleStringList.push_back(nullptr);
+
+		// execute program with arguments
+		errno = 0;
+		int result = execvp(cStyleStringList[0], cStyleStringList.data());
+
+		// If execvp failed, print error and terminate child process
+		if(result < 0)
+		{
+			std::string message{ shellName + ": " + command.name };
+			perror(message.c_str());
+			exit(EXIT_FAILURE);
+		}
+	}
 	else
-		inputLength = (int)inputString.length();
+	{
+		// This is the parent process, so wait for child to finish
+		errno = 0;
+		if(waitpid(child_pid, nullptr, WUNTRACED) < 0)
+		{
+			// Print error, if any
+			std::string message{ shellName + ": " + command.name };
+			perror(message.c_str());
+		}
+	}
+}
+
+//+------------------------\----------------------------------
+//|		   Utility		   |
+//\------------------------/----------------------------------
+void ClearTerminal()
+{
+	Command clearCommand;
+	clearCommand.name = "clear";
+	ExecuteExternalApp(clearCommand);
+}
+void PrintPrompt()
+{
+	std::string workingDirectoryString;
+	GetWorkingDirectory(workingDirectoryString);
+	std::cout << greenOnDefaultBold << shellName
+		<< defaultStyle << ':'
+		<< blueOnDefaultBold << '~' << workingDirectoryString
+		<< defaultStyle << "$ ";
+}
+void SeparateIntoCommand(const std::string& input, Command& commandOut)
+{
+	// Make sure output is empty
+	commandOut.name.clear();
+	commandOut.arguments.clear();
+
+	// Don't process empty input
+	if(input.empty())
+		return;
+
+	// Verify command is not too long
+	if(input.length() > MAX_COMMAND_LENGTH)
+	{
+		std::cerr << shellName << ": Exceeded maximum of " << MAX_COMMAND_LENGTH << " characters." << std::endl;
+		return;
+	}
+
+	// Convert length to signed integer so that (currentChar - currentWordStart + 1) works below
+	int inputLength = (int)input.length();
 
 	// Go char by char, finding the start and end of each word, and making a list
+	bool commandProcessed{ false };
 	for(int currentChar = 0, currentWordStart = 0; currentChar < inputLength; ++currentChar)
 	{
 		// If current character is whitespace
-		if(inputString[currentChar] == ' ' || inputString[currentChar] == '\t')
+		if(input[currentChar] == ' ' || input[currentChar] == '\t')
 		{
 			// If current word has any characters, record the word
-			if(currentChar > currentWordStart)
-				outputWordList.push_back(inputString.substr(currentWordStart, currentChar - currentWordStart));
+			int wordLength{ currentChar - currentWordStart };
+			if(wordLength > 0)
+			{
+				// Separate command from args
+				if(!commandProcessed)
+				{
+					commandOut.name = input.substr(currentWordStart, wordLength);
+					commandProcessed = true;
+				}
+				else
+					commandOut.arguments.push_back(input.substr(currentWordStart, wordLength));
+			}
 
 			// Next word needs to start on next character, any time a whitespace character is encountered, no matter if a word was recorded or not
 			currentWordStart = currentChar + 1;
@@ -137,10 +291,25 @@ void SeparateIntoWords(const std::string& inputString, std::vector<std::string>&
 		if(currentChar == inputLength - 1)
 		{
 			// If current word has any characters, record the word
-			if(currentChar - currentWordStart + 1 > 0)
-				outputWordList.push_back(inputString.substr(currentWordStart, currentChar - currentWordStart + 1));
+			int wordLength{ currentChar - currentWordStart + 1};
+			if(wordLength > 0)
+			{
+				// Separate command from args
+				if(!commandProcessed)
+				{
+					commandOut.name = input.substr(currentWordStart, wordLength);
+					commandProcessed = true;
+				}
+				else
+					commandOut.arguments.push_back(input.substr(currentWordStart, wordLength));
+			}
 		}
 	}
+}
+void PrintCommand(const Command& command)
+{
+	std::cout << command.name << ' ';
+	PrintWordList(command.arguments);
 }
 void PrintWordList(const std::vector<std::string>& wordList)
 {
@@ -149,54 +318,6 @@ void PrintWordList(const std::vector<std::string>& wordList)
 		std::cout << wordList[i];
 		if(i < wordList.size() - 1)
 			std::cout << ' ';
-	}
-}
-void ExecuteExternalApp(const std::vector<std::string>& pathAndArgs)
-{
-	if(pathAndArgs.empty())
-		return;
-
-	pid_t child_pid;
-	child_pid = fork();
-
-	// If fork failed, print error
-	if(child_pid < 0)
-	{
-		std::string message{ "Error creating child process to execute " + pathAndArgs[0] };
-		perror(message.c_str());
-	}
-
-	// If this is the child process, execute external app
-	if(child_pid == 0)
-	{
-		// Convert std::string list to c-style string list 
-		std::vector<char*> cStyleStringList;
-		for(auto const& word : pathAndArgs)
-			cStyleStringList.emplace_back(const_cast<char*>(word.c_str()));
-
-		// exec expects null-terminated array
-		cStyleStringList.push_back(nullptr);
-
-		// execute program with arguments
-		int result = execvp(cStyleStringList[0], cStyleStringList.data());
-
-		// If execvp failed, print error and terminate child process
-		if(result < 0)
-		{
-			std::string message{ "Error executing " + pathAndArgs[0] };
-			perror(message.c_str());
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		// This is the parent process, so wait for child to finish
-		if(waitpid(child_pid, nullptr, WUNTRACED) < 0)
-		{
-			// Print error, if any
-			std::string message{ "Error waiting for and reaping child process " + pathAndArgs[0] };
-			perror(message.c_str());
-		}
 	}
 }
 void GetWorkingDirectory(std::string& pathOutput)
@@ -212,3 +333,4 @@ void GetWorkingDirectory(std::string& pathOutput)
 	else
 		pathOutput = cwd;
 }
+
