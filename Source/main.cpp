6ab39f44b2b4ab4cfabe0b26d28f4040ad52fd43
DavidLeksen <lexellence@protonmail.com>
2019-12-20 05:01:47 -0500

    Changed my mind and removed CommandList class (it's simpler without it), added AddUniqueCommandToFront/GetCommandPtr functions instead; changed function groupings and group titles; moved DoShell code to main

--- Source/main.cpp
+++ Source/main.cpp
@@ -21,6 +21,7 @@ const std::string COMMAND_SEPARATOR_2{ ";" };
 const std::string REDIRECT_OUTPUT_OPERATOR{ ">" };
 const std::string REDIRECT_OUTPUT_APPEND_OPERATOR{ ">>" };
 const std::string REDIRECT_INPUT_OPERATOR{ "<" };
+const std::string PIPING_OPERATOR{ "<" };
 
 // Commands
 const std::string SHELL_NAME{ "lex" };
@@ -49,7 +50,6 @@ const std::string& USER_STYLE{ TEXT_STYLE_GREEN_ON_DEFAULT_BOLD };
 const std::string& DIRECTORY_STYLE{ TEXT_STYLE_BLUE_ON_DEFAULT_BOLD };
 const std::string& PUNCUATION_STYLE{ TEXT_STYLE_DEFAULT };
 
-void PrintWordList(const std::vector<std::string>& wordList);
 struct Command
 {
 	std::string name;
@@ -66,118 +66,95 @@ struct Command
 			return false;
 		return true;
 	}
-	friend std::ostream& operator<<(std::ostream& os, const Command& cmd)
-	{
-		os << cmd.name << ' ';
-		PrintWordList(cmd.arguments);
-		return os;
-	}
+	friend std::ostream& operator<<(std::ostream& os, const Command& cmd);
 };
 
-using CommandListIndex = std::list<Command>::size_type;
-class CommandList
-{
-public:
-	CommandList() = delete;
-	CommandList(unsigned maxCommands)
-		:m_maxCommands{ maxCommands } { }
-	const Command* GetCommandPtr(const std::string& indexString) const
-	{
-		try { 
-			return GetCommandPtr(std::stoul(indexString));
-		}
-		catch(const std::logic_error& e) { 
-			return nullptr; 
-		}
-	}
-	const Command* GetCommandPtr(CommandListIndex index) const
-	{
-		if(index >= m_commandList.size())
-			return nullptr;
-		else if(index == m_commandList.size() - 1)
-			return &m_commandList.back();
-		else if(index == 0)
-			return &m_commandList.front();
-		else
-		{
-			auto it{ m_commandList.begin() };
-			std::advance(it, index);
-			return &*it;
-		}
-	}	
-	const std::list<Command>& GetList() const
-	{
-		return m_commandList;
-	}
-	void Add(const Command& newCommand)
-	{
-		auto foundCommandIter{ std::find(m_commandList.begin(), m_commandList.end(), newCommand) };
-		if(foundCommandIter != m_commandList.end())
-		{
-			if(foundCommandIter != m_commandList.begin())
-				m_commandList.splice(m_commandList.begin(), m_commandList, foundCommandIter);
-		}
-		else
-		{
-			m_commandList.push_front(newCommand);
-			
-			// Trim
-			while(m_commandList.size() > m_maxCommands)
-				m_commandList.pop_back();
-		}
-	}
-	void Add(const CommandList& sourceList)
-	{
-		for(auto sourceIter{ sourceList.m_commandList.rbegin() }; sourceIter != sourceList.m_commandList.rend(); ++sourceIter)
-			Add(*sourceIter);
-	}
-	void Clear()
-	{
-		m_commandList.clear();
-	}
-	
-private:
-	std::list<Command> m_commandList;
-	CommandListIndex m_maxCommands;
-};
 // Supports cdl command
 std::string lastWorkingDirectory;
 
-// This is persistent between user entries
-CommandList historyList{ 10 };
-
-// This gets added to history and cleared for each user entry
-CommandList executedCommandList{ 1000 };
+// History
+using CommandListIndex = std::list<Command>::size_type;
+CommandListIndex HISTORY_MAX_SIZE{ 10 };
+std::list<Command> commandHistory;
+std::list<Command> executedCommands;
 
 //+------------------------\----------------------------------
-//|		   Commands		   |
+//|		    Parse		   |
+//\------------------------/----------------------------------
+void SeparateIntoWords(const std::string& input, std::vector<std::string>& wordListOut);
+void SeparateIntoCommands(const std::vector<std::string>& wordList, std::vector<Command>& commandListOut);
+
+//+------------------------\----------------------------------
+//|		   Execute		   |
 //\------------------------/----------------------------------
 void ExecuteCommand(const Command& command);
-void ChangeWorkingDirectory(std::string directory);
+const Command* GetCommandPtr(const std::list<Command>& commandList, const std::string& indexStr);
+const Command* GetCommandPtr(const std::list<Command>& commandList, CommandListIndex index);
 void ExecuteExternalApp(const Command& command);
+void AddUniqueCommandToFront(const Command& sourceCommand, std::list<Command>& destinationList);
 
 //+------------------------\----------------------------------
-//|		   Utility		   |
+//|		   Directory	   |
 //\------------------------/----------------------------------
-void ClearTerminal();
-void PrintPrompt();
-void SeparateIntoCommands(const std::vector<std::string>& wordList, std::vector<Command>& commandListOut);
-void SeparateIntoWords(const std::string& input, std::vector<std::string>& wordListOut);
-void PrintHistory();
-void GetUser(std::string& userOut);
+void ChangeWorkingDirectory(std::string directory);
 void ExpandDirectory(std::string& directory);
 void GetHomeDirectory(std::string& homeOut);
 void GetWorkingDirectory(std::string& pathOut);
 
 //+------------------------\----------------------------------
+//|		    Print		   |
+//\------------------------/----------------------------------
+void ClearTerminal();
+void PrintPrompt();
+void GetUser(std::string& userOut);
+void PrintHistory();
+void PrintWordList(const std::vector<std::string>& wordList);
+
+
+
+//+------------------------\----------------------------------
 //|			 Main		   |
 //\------------------------/----------------------------------
-void DoShell();
 int main()
 {
 	try
 	{
-		DoShell();
+		ClearTerminal();
+		GetWorkingDirectory(lastWorkingDirectory);
+		while(true)
+		{
+			PrintPrompt();
+
+			// Get list of commands from command-line
+			std::vector<Command> commands;
+			{
+				std::vector<std::string> wordList;
+				{
+					std::string inputLine;
+					getline(std::cin, inputLine);
+					SeparateIntoWords(inputLine, wordList);
+				}
+				SeparateIntoCommands(wordList, commands);
+			}
+
+			// Execute list of commands
+			for(Command cmd : commands)
+			{
+				if(cmd.name == QUIT_COMMAND_1 || cmd.name == QUIT_COMMAND_2)
+					return 0;
+
+				ExecuteCommand(cmd);
+			}
+
+			// Record executed commands in history, oldest to most recent
+			for(auto sourceIter{ executedCommands.rbegin() }; sourceIter != executedCommands.rend(); ++sourceIter)
+				AddUniqueCommandToFront(*sourceIter, commandHistory);
+			executedCommands.clear();
+
+			// Trim history
+			while(commandHistory.size() > HISTORY_MAX_SIZE)
+				commandHistory.pop_back();
+		}
 	}
 	catch(const std::exception & e)
 	{
@@ -186,46 +163,87 @@ int main()
 	}
 	return EXIT_SUCCESS;
 }
-void DoShell()
-{
-	ClearTerminal();
-	GetWorkingDirectory(lastWorkingDirectory);
-	while(true)
-	{
-		PrintPrompt();
 
-		// Get list of commands from command-line
-		std::vector<Command> commands;
+//+------------------------\----------------------------------
+//|		    Parse		   |
+//\------------------------/----------------------------------
+void SeparateIntoWords(const std::string& input, std::vector<std::string>& wordsOut)
+{
+	// Go char by char, finding the start and end of each word, and making a list
+	wordsOut.clear();
+	for(std::string::size_type currentChar = 0, currentWordStart = 0; currentChar < input.size(); ++currentChar)
+	{
+		// If current character is whitespace
+		if(input[currentChar] == WHITE_SPACE_CHAR_1 || input[currentChar] == WHITE_SPACE_CHAR_2)
 		{
-			// Get line of input, and tokenize
-			std::vector<std::string> wordList;
+			// If current word has any characters, record the word
+			if(currentChar > currentWordStart)
+				wordsOut.push_back(input.substr(currentWordStart, currentChar - currentWordStart));
+
+			// Next word needs to start on next character, any time a whitespace character is encountered, no matter if a word was recorded or not
+			currentWordStart = currentChar + 1;
+		}
+
+		// If we are at the last character
+		if(currentChar + 1 == input.size())
+		{
+			// If current word has any characters, record the word
+			if(currentChar + 1 > currentWordStart)
+				wordsOut.push_back(input.substr(currentWordStart, (currentChar + 1) - currentWordStart));
+		}
+	}
+}
+void SeparateIntoCommands(const std::vector<std::string>& wordList, std::vector<Command>& commandsOut)
+{
+	// Go word by word, finding the start and end of each command, and making a list
+	commandsOut.clear();
+	for(std::vector<std::string>::size_type currentWord = 0, currentCommandStart = 0; currentWord < wordList.size(); ++currentWord)
+	{
+		// If current word is command separator
+		if(wordList[currentWord] == COMMAND_SEPARATOR_1 || wordList[currentWord] == COMMAND_SEPARATOR_2)
+		{
+			// If current command has any words, record the command
+			if(currentWord > currentCommandStart)
 			{
-				std::string inputLine;
-				getline(std::cin, inputLine);
-				SeparateIntoWords(inputLine, wordList);
+				// Record command name
+				Command command;
+				command.name = wordList[currentCommandStart];
+
+				// Record arguments, if any
+				for(std::vector<std::string>::size_type i = currentCommandStart + 1; i < currentWord; ++i)
+					command.arguments.push_back(wordList[i]);
+
+				// Record command
+				commandsOut.push_back(command);
 			}
 
-			// Convert to list of commands
-			SeparateIntoCommands(wordList, commands);
+			// Next command needs to start on next word
+			currentCommandStart = currentWord + 1;
 		}
 
-		// Execute list of commands
-		for(Command cmd : commands)
+		// If we are at the last word
+		if(currentWord + 1 == wordList.size())
 		{
-			if(cmd.name == QUIT_COMMAND_1 || cmd.name == QUIT_COMMAND_2)
-				return;
+			// If current command has any words, record the command
+			if(currentWord + 1 > currentCommandStart)
+			{
+				// Record command name
+				Command command;
+				command.name = wordList[currentCommandStart];
 
-			ExecuteCommand(cmd);
+				// Record arguments, if any
+				for(std::vector<std::string>::size_type i = currentCommandStart + 1; i <= currentWord; ++i)
+					command.arguments.push_back(wordList[i]);
+
+				// Record command
+				commandsOut.push_back(command);
+			}
 		}
-
-		// Record in history
-		historyList.Add(executedCommandList);
-		executedCommandList.Clear();
 	}
 }
 
 //+------------------------\----------------------------------
-//|		   Commands		   |
+//|		   Execute		   |
 //\------------------------/----------------------------------
 void ExecuteCommand(const Command& command)
 {
@@ -254,7 +272,7 @@ void ExecuteCommand(const Command& command)
 		}
 		else
 		{
-			commandToExecutePtr = historyList.GetCommandPtr(command.arguments[0]);
+			commandToExecutePtr = GetCommandPtr(commandHistory, command.arguments[0]);
 			if(!commandToExecutePtr)
 			{
 				std::cerr << SHELL_NAME << ": " << EXECUTE_HISTORY_COMMAND << ": invalid index" << std::endl;
@@ -264,7 +282,7 @@ void ExecuteCommand(const Command& command)
 	}
 
 	// Record command
-	executedCommandList.Add(*commandToExecutePtr);
+	AddUniqueCommandToFront(*commandToExecutePtr, executedCommands);
 
 	// Execute command
 	if(commandToExecutePtr->name == CHANGE_DIRECTORY_COMMAND)
@@ -286,28 +304,30 @@ void ExecuteCommand(const Command& command)
 	else
 		ExecuteExternalApp(*commandToExecutePtr);
 }
-
-void ChangeWorkingDirectory(std::string directory)
+const Command* GetCommandPtr(const std::list<Command>& commandList, const std::string& indexStr)
 {
-	// Retain current working directory for cdl command
-	std::string savedWorkingDirectory;
-	GetWorkingDirectory(savedWorkingDirectory);
-
-	// Change directories
-	ExpandDirectory(directory);
-	errno = 0;
-	if(!chdir(directory.c_str()))
-	{
-		// Overwrite last directory if we successfully changed directories.
-		lastWorkingDirectory = savedWorkingDirectory;
+	try {
+		return GetCommandPtr(commandList, std::stoul(indexStr));
 	}
-	else
-	{
-		std::string message{ SHELL_NAME + ": " + CHANGE_DIRECTORY_COMMAND + ": \'" + directory + "\'" };
-		perror(message.c_str());
+	catch(const std::logic_error & e) {
+		return nullptr;
 	}
 }
-
+const Command* GetCommandPtr(const std::list<Command>& commandList, CommandListIndex index)
+{
+	if(index >= commandList.size())
+		return nullptr;
+	else if(index == commandList.size() - 1)
+		return &commandList.back();
+	else if(index == 0)
+		return &commandList.front();
+	else
+	{
+		auto it{ commandList.begin() };
+		std::advance(it, index);
+		return &*it;
+	}
+}
 void ExecuteExternalApp(const Command& command)
 {
 	if(command.name.empty())
@@ -376,139 +396,40 @@ void ExecuteExternalApp(const Command& command)
 		}
 	}
 }
+void AddUniqueCommandToFront(const Command& sourceCommand, std::list<Command>& destinationList)
+{
+	auto foundExistingCommandIter{ std::find(destinationList.begin(), destinationList.end(), sourceCommand) };
+	if(foundExistingCommandIter != destinationList.end())
+	{
+		if(foundExistingCommandIter != destinationList.begin())
+			destinationList.splice(destinationList.begin(), destinationList, foundExistingCommandIter);
+	}
+	else
+		destinationList.push_front(sourceCommand);
+}
 
 //+------------------------\----------------------------------
-//|		   Utility		   |
+//|		   Directory	   |
 //\------------------------/----------------------------------
-void ClearTerminal()
+void ChangeWorkingDirectory(std::string directory)
 {
-	Command clearCommand;
-	clearCommand.name = "clear";
-	ExecuteExternalApp(clearCommand);
-}
-void PrintPrompt()
-{
-	std::cout << SHELL_STYLE << SHELL_NAME;
+	// Retain current working directory for cdl command
+	std::string savedWorkingDirectory;
+	GetWorkingDirectory(savedWorkingDirectory);
+
+	// Change directories
+	ExpandDirectory(directory);
+	errno = 0;
+	if(!chdir(directory.c_str()))
 	{
-		std::string user;
-		GetUser(user);
-		if(!user.empty())
-			std::cout << PUNCUATION_STYLE << '(' << USER_STYLE << user << PUNCUATION_STYLE << ')';
+		// Overwrite last directory if we successfully changed directories.
+		lastWorkingDirectory = savedWorkingDirectory;
 	}
-	std::cout << PUNCUATION_STYLE << ':';
-	{
-		std::string workingDirectory;
-		GetWorkingDirectory(workingDirectory);
-
-		std::cout << DIRECTORY_STYLE << workingDirectory
-			<< PUNCUATION_STYLE << "$ " << TEXT_STYLE_DEFAULT;
-	}
-}
-void SeparateIntoCommands(const std::vector<std::string>& wordList, std::vector<Command>& commandsOut)
-{
-	// Go word by word, finding the start and end of each command, and making a list
-	commandsOut.clear();
-	for(std::vector<std::string>::size_type currentWord = 0, currentCommandStart = 0; currentWord < wordList.size(); ++currentWord)
-	{
-		// If current word is command separator
-		if(wordList[currentWord] == COMMAND_SEPARATOR_1 || wordList[currentWord] == COMMAND_SEPARATOR_2)
-		{
-			// If current command has any words, record the command
-			if(currentWord > currentCommandStart)
-			{
-				// Record command name
-				Command command;
-				command.name = wordList[currentCommandStart];
-
-				// Record arguments, if any
-				for(std::vector<std::string>::size_type i = currentCommandStart + 1; i < currentWord; ++i)
-					command.arguments.push_back(wordList[i]);
-
-				// Record command
-				commandsOut.push_back(command);
-			}
-
-			// Next command needs to start on next word
-			currentCommandStart = currentWord + 1;
-		}
-
-		// If we are at the last word
-		if(currentWord + 1 == wordList.size())
-		{
-			// If current command has any words, record the command
-			if(currentWord + 1 > currentCommandStart)
-			{
-				// Record command name
-				Command command;
-				command.name = wordList[currentCommandStart];
-
-				// Record arguments, if any
-				for(std::vector<std::string>::size_type i = currentCommandStart + 1; i <= currentWord; ++i)
-					command.arguments.push_back(wordList[i]);
-
-				// Record command
-				commandsOut.push_back(command);
-			}
-		}
-	}
-}
-void SeparateIntoWords(const std::string& input, std::vector<std::string>& wordsOut)
-{
-	// Go char by char, finding the start and end of each word, and making a list
-	wordsOut.clear();
-	for(std::string::size_type currentChar = 0, currentWordStart = 0; currentChar < input.size(); ++currentChar)
-	{
-		// If current character is whitespace
-		if(input[currentChar] == WHITE_SPACE_CHAR_1 || input[currentChar] == WHITE_SPACE_CHAR_2)
-		{
-			// If current word has any characters, record the word
-			if(currentChar > currentWordStart)
-				wordsOut.push_back(input.substr(currentWordStart, currentChar - currentWordStart));
-
-			// Next word needs to start on next character, any time a whitespace character is encountered, no matter if a word was recorded or not
-			currentWordStart = currentChar + 1;
-		}
-
-		// If we are at the last character
-		if(currentChar + 1 == input.size())
-		{
-			// If current word has any characters, record the word
-			if(currentChar + 1 > currentWordStart)
-				wordsOut.push_back(input.substr(currentWordStart, (currentChar + 1) - currentWordStart));
-		}
-	}
-}
-void PrintWordList(const std::vector<std::string>& wordList)
-{
-	for(std::vector<std::string>::size_type i = 0; i < wordList.size(); ++i)
-	{
-		std::cout << wordList[i];
-		if(i < wordList.size() - 1)
-			std::cout << ' ';
-	}
-}
-void PrintHistory()
-{
-	// Print history list in reverse so that most recent command is printed last
-	const std::list<Command>& cmdList{ historyList.GetList() };
-	if(cmdList.empty())
-	{
-		std::cout << SHELL_NAME << ": " << DISPLAY_HISTORY_COMMAND << ": empty" << std::endl;
-		return;
-	}
-
-	CommandListIndex i{ cmdList.size() - 1 };
-	for(auto cmdIter = cmdList.rbegin(); cmdIter != cmdList.rend(); ++cmdIter, --i)
-		std::cout << SHELL_NAME << ": ! " << i << ": " << *cmdIter << std::endl;
-}
-
-void GetUser(std::string& userOut)
-{
-	char* user{ getenv("USER") };
-	if(!user)
-		userOut.clear();
 	else
-		userOut = user;
+	{
+		std::string message{ SHELL_NAME + ": " + CHANGE_DIRECTORY_COMMAND + ": \'" + directory + "\'" };
+		perror(message.c_str());
+	}
 }
 void ExpandDirectory(std::string& directory)
 {
@@ -558,5 +479,71 @@ void GetWorkingDirectory(std::string& pathOut)
 	else
 		pathOut = cwd;
 }
+
+//+------------------------\----------------------------------
+//|		    Print		   |
+//\------------------------/----------------------------------
+void ClearTerminal()
+{
+	Command clearCommand;
+	clearCommand.name = "clear";
+	ExecuteExternalApp(clearCommand);
+}
+void PrintPrompt()
+{
+	std::cout << SHELL_STYLE << SHELL_NAME;
+	{
+		std::string user;
+		GetUser(user);
+		if(!user.empty())
+			std::cout << PUNCUATION_STYLE << '(' << USER_STYLE << user << PUNCUATION_STYLE << ')';
+	}
+	std::cout << PUNCUATION_STYLE << ':';
+	{
+		std::string workingDirectory;
+		GetWorkingDirectory(workingDirectory);
+
+		std::cout << DIRECTORY_STYLE << workingDirectory
+			<< PUNCUATION_STYLE << "$ " << TEXT_STYLE_DEFAULT;
+	}
+}
+void GetUser(std::string& userOut)
+{
+	char* user{ getenv("USER") };
+	if(!user)
+		userOut.clear();
+	else
+		userOut = user;
+}
+void PrintHistory()
+{
+	// Print history list in reverse so that most recent command is printed last
+	if(commandHistory.empty())
+	{
+		std::cout << SHELL_NAME << ": " << DISPLAY_HISTORY_COMMAND << ": empty" << std::endl;
+		return;
+	}
+
+	CommandListIndex i{ commandHistory.size() - 1 };
+	for(auto cmdIter = commandHistory.rbegin(); cmdIter != commandHistory.rend(); ++cmdIter, --i)
+		std::cout << SHELL_NAME << ": ! " << i << ": " << *cmdIter << std::endl;
+}
+std::ostream& operator<<(std::ostream& os, const Command& cmd)
+{
+	os << cmd.name << ' ';
+	PrintWordList(cmd.arguments);
+	return os;
+}
+void PrintWordList(const std::vector<std::string>& wordList)
+{
+	for(std::vector<std::string>::size_type i = 0; i < wordList.size(); ++i)
+	{
+		std::cout << wordList[i];
+		if(i < wordList.size() - 1)
+			std::cout << ' ';
+	}
+}
+
+
 
 
